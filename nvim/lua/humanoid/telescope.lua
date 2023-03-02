7c0b5aa7ccfd0b62322574fc2b99259993f211be
sinistertrance <115776632+sinistertrance@users.noreply.github.com>
2023-03-02 13:08:26 -0800

    Update telescope.lua

--- nvim/lua/humanoid/telescope.lua
+++ nvim/lua/humanoid/telescope.lua
@@ -10,12 +10,15 @@ require('telescope').setup{
   defaults = {
     -- Default configuration for telescope goes here:
     -- config_key = value,
+    prompt_prefix = " ",
+    --selection_caret = " ",
+    selection_caret = " ",
     mappings = {
       i = {
         -- map actions.which_key to <C-h> (default: <C-/>)
         -- actions.which_key shows the mappings for your picker,
         -- e.g. git_{create, delete, ...}_branch for the git_branches picker
-        ["<C-h>"] = "which_key"
+        --["<C-h>"] = "which_key"
       }
     }
   },
